f1028de27131df8f77e24e171a4719227457fe61
Stanley Pan <imoment.developer@gmail.com>
2022-01-20 15:03:18 -0500

    Initial Commit

---
+++ BankingUI/BankingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingUIApp.swift
+//  BankingUI
+//
+//  Created by Stanley Pan on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct BankingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
